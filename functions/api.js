f605e1b259f1cd9a48471b1aa7fabc7e9d766b2d
EndzyCodes <127296867+ReginioMarkGilbert@users.noreply.github.com>
2024-05-20 10:21:20 +0800

    -

--- functions/api.js
+++ functions/api.js
@@ -12,18 +12,18 @@ const app = express();
 // MongoDB connection
 const db = 'mongodb://localhost:27017/chainsawRegistration';
 
-mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log("Failed to connect to MongoDB", err));
+    .catch(err => console.log(err));
 
 // Routes
 // const applicationRoutes = require('./routes/applicationRoutes');
 // app.use('/api', applicationRoutes);
 
-// Start the server
+// // Start the server
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 app.use('/.netlify/functions/api', router);
 
